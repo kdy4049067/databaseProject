5bceacd3db68fbca785e6230ae1b90c0d53b2a73
daeyeon <kdy4410121@naver.com>
2024-12-03 15:07:06 +0900

    feat: userService 생성

---
+++ src/main/java/org/example/service/UserService.java
@@ -0,0 +1,20 @@
+package org.example.service;
+
+import org.example.entity.User;
+import org.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+
+
+}

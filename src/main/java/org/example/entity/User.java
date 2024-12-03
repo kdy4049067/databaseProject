bdc8e48da731134b69f75e04f51b97ca56a4d640
daeyeon <kdy4410121@naver.com>
2024-12-03 15:39:08 +0900

    feat: User 엔티티 데이터베이스와 연동

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,9 +1,39 @@
 package org.example.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.example.dto.UserDto;
+
+@Entity(name = "User")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class User {
 
-    private String id;
-    private String password;
+    public User(String uid, String password){
+        this.uid = uid;
+        this.password = password;
+    }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column
+    private String uid;
+    @Column
+    private String password;
+    @Column
+    private String role;
+
+    public UserDto toUserDto(){
+        return new UserDto(
+                this.id,
+                this.uid,
+                this.password,
+                this.role);
+    }
 
 }

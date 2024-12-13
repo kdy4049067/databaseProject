865ffb9a03e933d7255ca22d2d9349301a49c6cb
daeyeon <kdy4410121@naver.com>
2024-12-13 15:28:33 +0900

    feat: import 추가

--- src/main/java/org/example/Book/repository/BookRepository.java
+++ src/main/java/org/example/Book/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package org.example.Book.repository;
 
 import org.example.Book.domain.Book;
+import org.example.Book.dto.BookDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, String> {

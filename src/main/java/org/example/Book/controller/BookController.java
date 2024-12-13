cc72e4429034ce9b93c65643fdd14c1f08ccb8ac
daeyeon <kdy4410121@naver.com>
2024-12-13 21:02:37 +0900

    feat: getMapping 추가

--- src/main/java/org/example/Book/controller/BookController.java
+++ src/main/java/org/example/Book/controller/BookController.java
@@ -1,9 +1,11 @@
 package org.example.Book.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.Book.domain.Book;
 import org.example.Book.dto.BookDto;
 import org.example.Book.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,12 +14,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@Slf4j
+@Controller
 public class BookController {
 
     private final BookService bookService;
 
     public BookController(BookService bookService){
         this.bookService = bookService;
+    }
+
+    @GetMapping("/bookInsert")
+    public String showBookInsertForm() {
+        // bookInsert.html 페이지로 이동
+        return "/book/bookInsert";
     }
 
     @PostMapping("/book/insert")
@@ -28,7 +38,7 @@ public class BookController {
         String category = bookDto.category();
         String title = bookDto.title();
 
-        Book book = new Book(year, title, price, category, isbn);
+        Book book = new Book(isbn, title, price, category, year);
         bookService.insertBook(book);
 
         return "redirect:/book";
@@ -43,7 +53,7 @@ public class BookController {
     }
 
     @GetMapping("/book/update/{isbn}")
-    public String findUpdateBook(Model model, @PathVariable String isbn){
+    public String findUpdateBook(Model model, @PathVariable(name = "isbn") String isbn){
         Book book = bookService.findBookByIsbn(isbn);
         BookDto findBookDto = book.toBookDto();
         model.addAttribute("bookDto", findBookDto);
@@ -52,19 +62,20 @@ public class BookController {
     }
 
     @PostMapping("/book/update/{isbn}")
-    public String updateBook(@ModelAttribute BookDto bookDto, @PathVariable String isbn){
+    public String updateBook(@ModelAttribute BookDto bookDto, @PathVariable(name = "isbn") String isbn){
         Book book = bookService.findBookByIsbn(isbn);
-        bookService.updateBook(book, bookDto);
-
+        BookDto updatedBookDto = bookService.updateBook(book, bookDto);
+        if(updatedBookDto == null){
+            throw new IllegalArgumentException("업데이트 실패");
+        }
         return "redirect:/book";
     }
 
     @PostMapping("/book/delete/{isbn}")
-    public String deleteBook(@PathVariable String isbn){
+    public String deleteBook(@PathVariable(name = "isbn") String isbn){
         bookService.deleteBook(isbn);
 
         return "redirect:/book";
     }
-
 
 }

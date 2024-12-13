d4c11d01308a7ce436a549277814e5679e7d9eea
daeyeon <kdy4410121@naver.com>
2024-12-14 04:26:15 +0900

    feat: author 찾고 업데이트 하는 함수 생성

--- src/main/java/org/example/Author/service/AuthorService.java
+++ src/main/java/org/example/Author/service/AuthorService.java
@@ -1,42 +1,67 @@
 package org.example.Author.service;
 
+import jakarta.transaction.Transactional;
 import org.example.Author.domain.Author;
+import org.example.Author.dto.AuthorDto;
 import org.example.Author.repository.AuthorRepository;
+import org.example.Customer.domain.Customer;
+import org.example.PhoneCustomer.domain.PhoneCustomer;
+import org.example.Url.domain.Url;
+import org.example.Url.repository.UrlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthorService {
 
     @Autowired
     private AuthorRepository authorRepository;
+    @Autowired
+    private UrlRepository urlRepository;
 
-    // Author 저장 (Create)
-    public Author saveAuthor(Author author) {
-        return authorRepository.save(author);
+    public Url makeUrl(String urls){
+        return urlRepository.findUrlByUrl(urls);
     }
 
-    // Author 조회 (Read)
+    public AuthorDto saveAuthor(Author author) {
+        Author newAuthor = authorRepository.save(author);
+        return newAuthor.toAuthorDto();
+    }
+
     public Author getAuthor(String name) {
         return authorRepository.findAuthorByName(name);
     }
 
-    // Author 수정 (Update)
-    public Author updateAuthor(String name, Author newAuthor) {
-        Optional<Author> existingAuthor = authorRepository.findById(name);
-        if (existingAuthor.isPresent()) {
-            Author author = existingAuthor.get();
-            author.setUrl(newAuthor.getUrl());
-            author.setAwards(newAuthor.getAwards());
-            author.setBooks(newAuthor.getBooks());
-            return authorRepository.save(author);
-        }
-        return null; // 혹은 예외 처리
+    public List<AuthorDto> findAllAuthor(){
+        List<Author> author = authorRepository.findAll();
+        return author.stream()
+                .map(Author::toAuthorDto)
+                .collect(Collectors.toList());
     }
 
-    // Author 삭제 (Delete)
+    @Transactional
+    public AuthorDto updateAuthor(Author author, AuthorDto newAuthorDto) {
+        if (!author.getName().equals(newAuthorDto.name()) && isNameExist(newAuthorDto.name())) {
+            throw new IllegalArgumentException("이미 존재하는 name입니다.");
+        }
+        Url url = urlRepository.findUrlByUrl(newAuthorDto.urls());
+        author.setUrl(url);
+        author.setName(newAuthorDto.name());
+        author.setUrls(newAuthorDto.urls());
+
+        return author.toAuthorDto();
+    }
+
     public void deleteAuthor(String name) {
         authorRepository.deleteAuthorByName(name);
     }
+
+    public boolean isNameExist(String name) {
+        return authorRepository.existsByName(name);
+    }
+
 }

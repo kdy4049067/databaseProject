ea59807f42c8ae119f5cc1da35acabbd2c22292c
daeyeon <kdy4410121@naver.com>
2024-12-13 14:35:55 +0900

    feat: setter 롬복 추가

--- src/main/java/org/example/Book/domain/Book.java
+++ src/main/java/org/example/Book/domain/Book.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.example.Book.dto.BookDto;
 import org.example.User.dto.UserDto;
 
@@ -11,6 +12,7 @@ import org.example.User.dto.UserDto;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class Book {
 
     @Id

d2051449e729bb9cec86c1d712a4b8eacd2c2ef4
daeyeon <kdy4410121@naver.com>
2024-12-14 04:39:29 +0900

    feat: shoppingBasket 저장소 생성

---
+++ src/main/java/org/example/ShoppingBasket/repository/ShoppingBasketRepository.java
@@ -0,0 +1,10 @@
+package org.example.ShoppingBasket.repository;
+
+import org.example.ShoppingBasket.ShoppingBasket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoppingBasketRepository extends JpaRepository<ShoppingBasket, String> {
+
+    public ShoppingBasket findShoppingBasketByBasketId(String basketId);
+
+}

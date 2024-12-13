cba84131772b88b7527d17f808b304a2675e15d8
daeyeon <kdy4410121@naver.com>
2024-12-14 04:25:33 +0900

    feat: 패키지 변경

--- src/main/java/org/example/Author/dto/AuthorDto.java
+++ src/main/java/org/example/Author/dto/AuthorDto.java
@@ -1,11 +1,10 @@
 package org.example.Author.dto;
 
-import org.example.Award.Award;
+import org.example.Award.domain.Award;
 import org.example.Book.domain.Book;
-import org.example.Url.Url;
+import org.example.Url.domain.Url;
 
-import java.net.URL;
 import java.util.List;
 
-public record AuthorDto(String name, Url url, List<Award> awards, List<Book> books) {
+public record AuthorDto(String name, String urls, Url url) {
 }

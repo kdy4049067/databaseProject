708d397dfaabe62ef757d23a9d7452f34557d7bb
daeyeon <kdy4410121@naver.com>
2024-12-13 14:19:41 +0900

    feat: 모든 book 반환하는 함수 생성

--- src/main/java/org/example/Book/service/BookService.java
+++ src/main/java/org/example/Book/service/BookService.java
@@ -4,6 +4,10 @@ import org.example.Book.domain.Book;
 import org.example.Book.dto.BookDto;
 import org.example.Book.repository.BookRepository;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BookService {
 
     private final BookRepository bookRepository;
@@ -15,6 +19,13 @@ public class BookService {
     public BookDto insertBook(Book book){
         Book newBook = bookRepository.save(book);
         return newBook.toBookDto();
+    }
+
+    public List<BookDto> findAllBook(){
+        List<Book> book = bookRepository.findAll();
+        return book.stream()
+                .map(Book::toBookDto)
+                .collect(Collectors.toList());
     }
 
 }

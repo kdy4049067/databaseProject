8872f8c396b1f7edbc5fb6b8b5b952bf6f1461b1
daeyeon <kdy4410121@naver.com>
2024-12-14 04:33:51 +0900

    feat: contains repository에 필요한 함수 정의

---
+++ src/main/java/org/example/Contains/repository/ContainsRepository.java
@@ -0,0 +1,13 @@
+package org.example.Contains.repository;
+
+import org.example.Book.domain.Book;
+import org.example.Contains.domain.Contains;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContainsRepository extends JpaRepository<Contains, Long> {
+    public Contains findContainsByBook(String bookIsbn);
+    public boolean existsContainsByBook(Book book);
+    public void deleteContainsByBook(String bookIsbn);
+}

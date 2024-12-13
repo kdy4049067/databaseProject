86a67ec50d266b2a5d652d48d85b6ca8169a3a4c
daeyeon <kdy4410121@naver.com>
2024-12-13 21:06:05 +0900

    feat: shoppingBasket 참조 관계 반영해서 생성

---
+++ src/main/java/org/example/ShoppingBasket/ShoppingBasket.java
@@ -0,0 +1,25 @@
+package org.example.ShoppingBasket;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import org.example.Book.domain.Book;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class ShoppingBasket {
+
+    @Id
+    private String basketId;  // 기본키
+
+    private LocalDateTime orderDate;
+
+    @OneToMany(mappedBy = "shoppingBasket")
+    private List<Book> books = new ArrayList<>();
+
+    // 기본 생성자, getter, setter
+}
+

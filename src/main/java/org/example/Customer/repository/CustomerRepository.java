d6ac68f491c0de3241cb65856f0b345f13cac112
daeyeon <kdy4410121@naver.com>
2024-12-14 04:35:27 +0900

    feat: customer repository 생성

---
+++ src/main/java/org/example/Customer/repository/CustomerRepository.java
@@ -0,0 +1,17 @@
+package org.example.Customer.repository;
+
+import org.example.Book.domain.Book;
+import org.example.Customer.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+
+    public boolean existsByEmail(String email);
+
+    public Customer findCustomerByEmail(String email);
+
+    public void deleteCustomerByEmail(String email);
+
+}

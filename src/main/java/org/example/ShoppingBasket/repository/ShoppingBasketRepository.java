042a19bac22eb9d13cd16a8b2fe318c7831aa627
daeyeon <kdy4410121@naver.com>
2024-12-14 14:44:54 +0900

    feat: customer의 email을 통한 장바구니 찾는 함수 정의

--- src/main/java/org/example/ShoppingBasket/repository/ShoppingBasketRepository.java
+++ src/main/java/org/example/ShoppingBasket/repository/ShoppingBasketRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ShoppingBasketRepository extends JpaRepository<ShoppingBasket, String> {
 
     public ShoppingBasket findShoppingBasketByBasketId(String basketId);
-
+    public ShoppingBasket findShoppingBasketByCustomer(String email);
 }

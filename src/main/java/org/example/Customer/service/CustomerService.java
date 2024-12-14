2a7ef292de614832913ed174ef1ff2d4e6dc3f2c
daeyeon <kdy4410121@naver.com>
2024-12-14 14:34:17 +0900

    feat: 검색 기능, 로그인 기능 추가

--- src/main/java/org/example/Customer/service/CustomerService.java
+++ src/main/java/org/example/Customer/service/CustomerService.java
@@ -1,6 +1,12 @@
 package org.example.Customer.service;
 
 import jakarta.transaction.Transactional;
+import org.example.Author.domain.Author;
+import org.example.Author.repository.AuthorRepository;
+import org.example.Award.domain.Award;
+import org.example.Award.repository.AwardRepository;
+import org.example.Book.domain.Book;
+import org.example.Book.repository.BookRepository;
 import org.example.Customer.domain.Customer;
 import org.example.Customer.dto.CustomerDto;
 import org.example.Customer.repository.CustomerRepository;
@@ -9,19 +15,26 @@ import org.example.PhoneCustomer.repository.PhoneCustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class CustomerService {
-    @Autowired
     private final PhoneCustomerRepository phoneCustomerRepository;
     private final CustomerRepository customerRepository;
+    private final AuthorRepository authorRepository;
+    private final AwardRepository awardRepository;
+    private final BookRepository bookRepository;
 
-    public CustomerService(PhoneCustomerRepository phoneCustomerRepository, CustomerRepository customerRepository){
+
+    public CustomerService(PhoneCustomerRepository phoneCustomerRepository, CustomerRepository customerRepository, AuthorRepository authorRepository, AwardRepository awardRepository, BookRepository bookRepository){
         this.phoneCustomerRepository = phoneCustomerRepository;
         this.customerRepository = customerRepository;
+        this.authorRepository = authorRepository;
+        this.awardRepository = awardRepository;
+        this.bookRepository = bookRepository;
     }
 
     public PhoneCustomer makePhoneCustomer(String phone){
@@ -67,6 +80,45 @@ public class CustomerService {
     @Transactional
     public void deleteCustomer(String email) {
         customerRepository.deleteCustomerByEmail(email);
+    }
+
+    public List<Book> findBooksByAuthorName(String authorName) {
+        List<Author> authors = authorRepository.findAuthorsByName(authorName);
+
+        List<Book> books = authors.stream()
+                .flatMap(author -> author.getBooks().stream())
+                .distinct()
+                .collect(Collectors.toList());
+
+        return books;
+    }
+
+    public List<Book> findBooksByAwardName(String awardName) {
+        List<Award> awards = awardRepository.findAwardsByName(awardName);
+        List<Book> books = new ArrayList<>();
+        for(Award award : awards){
+            String bookIsbn = award.getBook().getIsbn();
+            Book book = bookRepository.findBookByIsbn(bookIsbn);
+
+            if(book != null)
+                books.add(book);
+        }
+        return books;
+    }
+
+    public List<Book> findBooksByTitle(String title){
+        List<Book> books = bookRepository.findBooksByTitle(title);
+
+        return books;
+    }
+
+    public boolean login(String email, String phone){
+        Customer customer = customerRepository.findCustomerByEmail(email);
+        if(customer == null) return false;
+        if(!customer.getPhone().equals(phone)){
+            return false;
+        }
+        return true;
     }
 
 }

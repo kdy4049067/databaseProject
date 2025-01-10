95c45adf13c1e4661cbe5763b9ba2e9270891eb0
daeyeon <kdy4410121@naver.com>
2025-01-10 15:52:47 +0900

    fix: 네이밍 수정

--- src/main/java/org/example/Customer/dto/CustomerDto.java
+++ src/main/java/org/example/Customer/dto/CustomerDto.java
@@ -2,5 +2,5 @@ package org.example.Customer.dto;
 
 import org.example.PhoneCustomer.domain.PhoneCustomer;
 
-public record CustomerDto(String email, String phone, PhoneCustomer phoneCustomer) {
+public record CustomerDto(String email, String phone, String phoneCustomerPhone) {
 }

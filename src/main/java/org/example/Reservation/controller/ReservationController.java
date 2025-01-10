493b28912db95bd331649fd0e93d57379e8ff198
daeyeon <kdy4410121@naver.com>
2025-01-10 16:17:56 +0900

    feat: 예약 변경 취소 기능 추가

--- src/main/java/org/example/Reservation/controller/ReservationController.java
+++ src/main/java/org/example/Reservation/controller/ReservationController.java
@@ -3,6 +3,8 @@ package org.example.Reservation.controller;
 import jakarta.persistence.Column;
 import org.example.Book.domain.Book;
 import org.example.Book.repository.BookRepository;
+import org.example.Customer.domain.Customer;
+import org.example.Customer.service.CustomerService;
 import org.example.Reservation.domain.Reservation;
 import org.example.Reservation.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,29 +26,66 @@ public class ReservationController {
 
     @Autowired
     private ReservationService reservationService;
+    @Autowired
+    private CustomerService customerService;
 
-    @GetMapping("/customer/{email}/book/showAllBooks")
-    public String showAllBooks(@PathVariable String email, Model model) {
-        List<Book> books = bookRepository.findAll();
+    @GetMapping("/reservation/{email}")
+    public String viewReservations(@PathVariable("email") String email, Model model) {
+        Customer customer = customerService.findCustomerByEmail(email);
+        Reservation reservation = reservationService.findReservation(customer);
+        List<Book> books = reservation.getBooks();
+        model.addAttribute("reservations", reservation);
         model.addAttribute("books", books);
-        model.addAttribute("email", email);
-        return "book-list";
+        return "reservation-list";
     }
 
+
     @PostMapping("/customer/{email}/book/reserve")
-    public String reserveBook(@PathVariable String email,
-                              @RequestParam String isbn,
-                              @RequestParam String pickupTime) {
+    public String reserveBook(@PathVariable("email") String email,
+                              @RequestParam(name="isbn") String isbn,
+                              @RequestParam(name="pickupTime") String pickupTime,
+                              Model model) {
         LocalDateTime pickupDateTime = LocalDateTime.parse(pickupTime);
 
         try {
             reservationService.reserveBooks(email, isbn, pickupDateTime);
-            return "redirect:/customer/" + email + "/reservations";
+            return "redirect:/reservation/" + email;
         } catch (IllegalArgumentException e) {
+            model.addAttribute("errorMessage", "이미 존재하는 Reservation입니다.");
             return "error";
         }
     }
 
+    @PostMapping("/reservation/{id}/{title}/cancel")
+    public String cancelReservation(@PathVariable("id") Long id,
+                                    @PathVariable("title")String title,
+                                    Model model) {
+        Reservation reservation = reservationService.findReservationById(id);
+        Customer customer = reservation.getCustomer();
+        try {
+            reservationService.cancelReservation(id, title);
+            return "redirect:/reservation/" + customer.getEmail();
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("errorMessage", "cancle 오류");
+            return "error";
+        }
+    }
+
+    @PostMapping("/reservation/{id}/update")
+    public String updatePickupTime(@PathVariable("id") Long id,
+                                   @RequestParam(name="newPickupTime") String newPickupTime,
+                                   Model model) {
+        LocalDateTime newTime = LocalDateTime.parse(newPickupTime);
+        Reservation reservation = reservationService.findReservationById(id);
+        Customer customer = reservation.getCustomer();
+        try {
+            reservationService.updatePickupTime(id, newTime);
+            return "redirect:/reservation/" + customer.getEmail();
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("errorMessage", "업데이트 실패.");
+            return "error";
+        }
+    }
 
 }
 
--- src/main/java/org/example/Reservation/repository/ReservationRepository.java
+++ src/main/java/org/example/Reservation/repository/ReservationRepository.java
@@ -1,11 +1,20 @@
 package org.example.Reservation.repository;
 
+import org.example.Book.domain.Book;
 import org.example.Customer.domain.Customer;
 import org.example.Reservation.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    public Reservation findReservationById(Long id);
+    boolean existsByCustomerAndBooksContaining(Customer customer, Book book);
+    Reservation findByCustomer(Customer customer);
     Reservation findReservationByCustomer(Customer customer);
+    boolean existsByPickupTimeBetween(LocalDateTime localDateTime, LocalDateTime localDateTime1);
 }
